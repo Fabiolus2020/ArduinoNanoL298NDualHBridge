c2fae2f1512181cfff012c917e4ed076ea84f6cc
Fabiolus2020 <76980761+Fabiolus2020@users.noreply.github.com>
2021-02-05 09:40:34 -0500

    Update 1JoyL298NDualHBridge2motorsVERSION2.ino

--- 1JoyL298NDualHBridge2motorsVERSION2.ino
+++ 1JoyL298NDualHBridge2motorsVERSION2.ino
@@ -1,4 +1,7 @@
 /*
+ * Fabiolus
+ * 2020/07/04
+ * the_fabiolous@hotmail.com
    This is a dual axis joystick skitch for Arduino.
    The position of joystick is read and displayed on serial monitor
 */
@@ -10,12 +13,12 @@ int y_pos;
 
 
 //Motor Pins
-int EN_A = 11;      //Enable pin for first motor
-int IN1 = 9;       //control pin for first motor
-int IN2 = 8;       //control pin for first motor
-int IN3 = 7;        //control pin for second motor
-int IN4 = 6;        //control pin for second motor
-int EN_B = 10;      //Enable pin for second motor
+int EN_A = 10;      //Enable pin for first motor
+int IN1 = 4;       //control pin for first motor
+int IN2 = 5;       //control pin for first motor
+int IN3 = 6;        //control pin for second motor
+int IN4 = 7;        //control pin for second motor
+int EN_B = 3;      //Enable pin for second motor
 
 //Initializing variables to store data
 int motor_speed1 = 0;
@@ -35,10 +38,6 @@ void setup() {
   //Initializng the joystick pins as input
   pinMode (x_key, INPUT) ;
   pinMode (y_key, INPUT) ;
-
-
-
-
 
 }
 
@@ -83,44 +82,41 @@ void loop() {
     digitalWrite(IN4, LOW);
   }
 
-    if (y_pos > 525) {    //Rotating direction FORWARD and BACKWARD BOTH MOTORS PUSHING JOYSTICK RIGHT
-     motor_speed1 = map(y_pos, 525, 1023, 0, 255);   //Mapping the values to 0-255 to move the motor
+  if (y_pos > 525) {    //Rotating direction FORWARD and BACKWARD BOTH MOTORS PUSHING JOYSTICK RIGHT
+    motor_speed1 = map(y_pos, 525, 1023, 0, 255);   //Mapping the values to 0-255 to move the motor
     motor_speed2 = map(y_pos, 525, 1023, 0, 255);   //Mapping the values to 0-255 to move the motor
     digitalWrite(IN1, LOW);
     digitalWrite(IN2, HIGH);
     analogWrite(EN_A, motor_speed1);
 
-     digitalWrite(IN3, LOW);
-     digitalWrite(IN4, HIGH);
+    digitalWrite(IN3, LOW);
+    digitalWrite(IN4, HIGH);
     analogWrite(EN_B, motor_speed2);
-   }
+  }
 
-    else if (y_pos < 460) {    //Rotating direction FORWARD and BACKWARD BOTH MOTORS PUSHING JOYSTICK RIGHT
-     motor_speed1 = map(y_pos, 460, 0, 0, 255);   //Mapping the values to 0-255 to move the motor
+  else if (y_pos < 460) {    //Rotating direction FORWARD and BACKWARD BOTH MOTORS PUSHING JOYSTICK RIGHT
+    motor_speed1 = map(y_pos, 460, 0, 0, 255);   //Mapping the values to 0-255 to move the motor
     motor_speed2 = map(y_pos, 460, 0, 0, 255);   //Mapping the values to 0-255 to move the motor
     digitalWrite(IN1, HIGH);
     digitalWrite(IN2, LOW);
     analogWrite(EN_A, motor_speed1);
 
-     digitalWrite(IN3, HIGH);
-     digitalWrite(IN4, LOW);
+    digitalWrite(IN3, HIGH);
+    digitalWrite(IN4, LOW);
     analogWrite(EN_B, motor_speed2);
-   }
+  }
 
 
-
-
-
- //   // Adjust to prevent "buzzing" at very low speed
+  //   // Adjust to prevent "buzzing" at very low speed
 
   if (motor_speed1 < 20)motor_speed1 = 0;
   if (motor_speed2 < 20)motor_speed2 = 0;
 
 
-//  Serial.print("X: ");
-//  Serial.print(x_pos); //print x
+  //  Serial.print("X: ");
+  //  Serial.print(x_pos); //print x
 
-//  Serial.print(" Y: ");
-//  Serial.println(y_pos);// print y
+  //  Serial.print(" Y: ");
+  //  Serial.println(y_pos);// print y
 
 }
